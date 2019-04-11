5a9c725c4090d51a370d8f9b1f3d39e5c1581fed
axercos <nexos18.em@gmail.com>
2019-04-11 15:05:35 -0500

    Comit Ejercicio 2
    Segundo ccomit

--- 01_modulo/Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.java
+++ 01_modulo/Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.ejercicio2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtnum1)
+    EditText txtnum1;
+    @BindView(R.id.txtnum2)
+    EditText txtnum2;
+
+    @BindView(R.id.rbresta)
+    RadioButton rbresta;
+    @BindView(R.id.rbsuma)
+    RadioButton rbsuma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btncalculo)
+    public void hacerCalculo(){
+        Toast.makeText(this,"Presiones", Toast.LENGTH_LONG).show();
     }
 }
