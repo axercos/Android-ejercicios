ec8424259a8cd8cd6585048fdae36774a0b63804
axercos <nexos18.em@gmail.com>
2019-05-14 16:48:52 -0500

    Ejercicio 5 Completo
    ejercicio mediante el cual un spinner el estilo de la letra de un area de texto cambia

--- 01_modulo/Ejercicio5/app/src/main/java/com/example/ejercicio5/MainActivity.java
+++ 01_modulo/Ejercicio5/app/src/main/java/com/example/ejercicio5/MainActivity.java
@@ -8,30 +8,31 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //EditText txt1;
-    //Spinner sp1;
+    EditText txt1;
+    Spinner sp1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //txt1=(EditText) findViewById(R.id.txt1);
-        //sp1=(Spinner) findViewById(R.id.sp1);
+        txt1=(EditText) findViewById(R.id.txt1);
+        sp1=(Spinner) findViewById(R.id.sp1);
 
         //
-        String []opciones={"Negrita","Cursiva"};
+        String []opciones={"Normal","Negrita","Cursiva","Cursiva y Negrita"};
 
         //adapter
 
         ArrayAdapter<String> adaptador=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
 
         //modelos
-        //sp1.setAdapter(adaptador);
+        sp1.setAdapter(adaptador);
 
         sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
            @Override
@@ -40,8 +41,10 @@ public class MainActivity extends AppCompatActivity {
                String opcion=sp1.getSelectedItem().toString();
                switch (opcion){
                    case "Negrita": txt1.setTypeface(null, Typeface.BOLD);
-                   break;;
+                   break;
                    case "Cursiva": txt1.setTypeface(null, Typeface.ITALIC);
+                   break;
+                   case "Cursiva y Negrita": txt1.setTypeface(null, Typeface.BOLD_ITALIC);
                    break;
                    default:
                        txt1.setTypeface(null, Typeface.NORMAL);
